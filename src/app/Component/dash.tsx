2d0c865bc746b9f4b671a28d463e5c57541e726e
Paulo Maza <paa.hueson123@gmail.com>
2024-05-31 01:33:49 -0600

    cambios

--- src/app/Component/dash.tsx
+++ src/app/Component/dash.tsx
@@ -19,7 +19,7 @@ export default function Dash() {
     const [temas, setTemas] = useState<Tema[]>([]);
 
     useEffect(() => {
-        fetch("https://t4-01-repositorio-del-proyecto2.vercel.app")
+        fetch("http://localhost:3100/foros")
             .then(response => response.json())
             .then(data => setTemas(data))
             .catch(error => console.error('Error fetching data:', error));
